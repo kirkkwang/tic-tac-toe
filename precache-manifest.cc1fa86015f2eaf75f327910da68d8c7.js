64b89258bbaf69337a2a62314943c25ba6605264
kirkkwang <k3wang@gmail.com>
2022-01-25 20:52:38 -0800

    Updates

---
+++ precache-manifest.cc1fa86015f2eaf75f327910da68d8c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce603acc22f2e7173a57fc0b06dcd787",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "398fb8397a39434995e2",
+    "url": "/tic-tac-toe/static/css/main.d823175c.chunk.css"
+  },
+  {
+    "revision": "a06eb0cf4958763fc45e",
+    "url": "/tic-tac-toe/static/js/2.83b629c2.chunk.js"
+  },
+  {
+    "revision": "398fb8397a39434995e2",
+    "url": "/tic-tac-toe/static/js/main.86e0b2c6.chunk.js"
+  },
+  {
+    "revision": "9728fa41a930f12fdc87",
+    "url": "/tic-tac-toe/static/js/runtime~main.f1e29306.js"
+  }
+]);
